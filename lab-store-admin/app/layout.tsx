f07e6d01a784e9bd655ef2672358759a4b8ff605
henriqueponts <henriquearaujopontes@gmail.com>
2025-03-10 22:15:50 -0300

    Revert "Implementação do Clerk (Parte 1)"
    This reverts commit f509aa0dfea35d62669452561a59efd1b0b898df.

--- lab-store-admin/app/layout.tsx
+++ lab-store-admin/app/layout.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {
-  ClerkProvider,
-} from '@clerk/nextjs'
-import { ptBR } from '@clerk/localizations'
-import './globals.css'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,12 +23,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider  localization={ptBR}>
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         {children}
       </body>
     </html>
-    </ClerkProvider>
   );
 }
